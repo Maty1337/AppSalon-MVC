e1f722c6a3ec697daf7eb005abec6c4718f0b916
Maty1337 <matidavidbuena@gmail.com>
2025-09-16 15:59:14 -0300

    feature(vista): Creacion de la vista en la pagina Login(crear-cuenta-recuperar-cuenta) y ajustes en LoginController y estilos SCSS

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,25 +2,40 @@
 
 namespace Controllers;
 
+use MVC\Router;
 
 class LoginController {
-    public static function login() {
-        echo "Desde el controlador de Login";
+    public static function login(Router $router) {
+        $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Crear una nueva instancia de usuario
+            // debuguear($_POST);
+        }
+
+        $router->render('auth/login', [
+            'alertas' => $alertas
+        ]);
+        
     }
 
     public static function logout() {
         echo "Cerrar Sesión";
     }
 
-    public static function olvide() {
-        echo "Recuperar Password";
+    public static function olvide(Router $router) {
+        $router->render('auth/olvide-password', [
+            
+        ]);
     }
 
     public static function recuperar() {
         echo "Colocar nuevo password";
     }
 
-    public static function crear() {
-        echo "Crear una cuenta";
+    public static function crear(Router $router) {
+        $router->render('auth/crear-cuenta', [
+            
+        ]);
     }
 }
