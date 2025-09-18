4eb344b2f00d919ce89dec03a300eba348b9f587
Maty1337 <matidavidbuena@gmail.com>
2025-09-18 18:28:21 -0300

    feature(U-Create): Creacion y verificacion de usuario por token

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -2,10 +2,16 @@
 
 namespace Controllers;
 
+use Model\Usuario;
 use MVC\Router;
+use Classes\Email;
 
-class LoginController {
-    public static function login(Router $router) {
+
+
+class LoginController
+{
+    public static function login(Router $router)
+    {
         $alertas = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -16,26 +22,99 @@ class LoginController {
         $router->render('auth/login', [
             'alertas' => $alertas
         ]);
-        
     }
 
-    public static function logout() {
+    public static function logout()
+    {
         echo "Cerrar Sesión";
     }
 
-    public static function olvide(Router $router) {
-        $router->render('auth/olvide-password', [
-            
-        ]);
+    public static function olvide(Router $router)
+    {
+        $router->render('auth/olvide-password', []);
     }
 
-    public static function recuperar() {
+    public static function recuperar()
+    {
         echo "Colocar nuevo password";
     }
 
-    public static function crear(Router $router) {
-        $router->render('auth/crear-cuenta', [
+    public static function crear(Router $router)
+    {
+
+        $usuario = new Usuario($_POST);
+
+        //Alertas vacías
+        $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $usuario->sincronizar($_POST);//uso de sincronizar para llenar el objeto usuario con los datos del  formulario   
+            $alertas = $usuario->validarNuevaCuenta(); 
             
+            //Revisar que alertas esté vacío
+            if (empty($alertas)) {
+                $resultado = $usuario->existeUsuario();
+
+                if($resultado->num_rows) {
+                    $alertas = Usuario::getAlertas();
+                }else{
+                    //Hashear el password
+                    $usuario->hashPassword();
+
+                    //Generar un token único
+                    $usuario->crearToken();
+
+                    //Email de confirmación
+                    $email = new Email($usuario->email, $usuario->nombre, $usuario->token);
+
+                    //Enviar el email
+                    $email->enviarConfirmacion();
+
+                    //Crear el usuario
+                    $resultado = $usuario->guardar();
+
+                    if ($resultado) {
+                        header('Location: /mensaje');
+                    }
+                }
+            }
+        }
+
+
+        $router->render('auth/crear-cuenta', [
+            'usuario'=>$usuario,
+            'alertas' => $alertas
+        ]);
+    }
+
+    public static function mensaje(Router $router){
+        $router->render('auth/mensaje', []);
+    }
+
+    public static function confirmar(Router $router){
+        $alertas = [];
+
+        $token = s($_GET['token']);
+        
+        $usuario = Usuario::where('token', $token);
+        
+        if(empty($usuario)){
+            //Mostrar mensaje de error
+            Usuario::setAlerta('error', 'Token no válido');
+        }else{
+            //Modificar a usuario confirmado
+            $usuario->confirmado = "1";
+            $usuario->token = null;
+            $usuario->guardar();    
+            Usuario::setAlerta('exito', 'Cuenta comprobada correctamente');
+        }
+        //Obtener alertas
+        $alertas = Usuario::getAlertas();
+
+
+        //Renderizar la vista
+        $router->render('auth/confirmar-cuenta', [
+            'alertas' => $alertas
         ]);
     }
 }
--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -1,6 +1,8 @@
 <h1 class="nombre-pagina">Crear cuenta</h1>
 <p class="descripcion-pagina">Llena el siguiente formulario para crear una cuenta</p>
 
+<?php include_once __DIR__ . '/../templates/alertas.php'; ?>
+
 <form class="formulario" method="POST" action="/crear-cuenta">
     <div class="campo">
         <label for="nombre">Nombre</label>
@@ -9,6 +11,7 @@
             id="nombre"
             name="nombre"
             placeholder="Tu Nombre"
+            value="<?php echo s($usuario->nombre); ?>"
         />
     </div>
 
@@ -19,6 +22,7 @@
             id="apellido"
             name="apellido"
             placeholder="Tu Apellido"
+            value="<?php echo s($usuario->apellido); ?>"
         />
     </div>
 
@@ -29,6 +33,7 @@
             id="telefono"
             name="telefono"
             placeholder="Tu Telefono"
+            value="<?php echo s($usuario->telefono); ?>"
         />
     </div>
 
@@ -39,6 +44,7 @@
             id="email"
             name="email"
             placeholder="Tu E-mail"
+            value="<?php echo s($usuario->email); ?>"
         />
     </div>
 
