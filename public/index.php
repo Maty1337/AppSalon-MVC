3193f01b5838de50b0e8f40cc213e11fe8018579
Matias Buenaventura <132623948+Maty1337@users.noreply.github.com>
2025-09-22 17:44:18 -0300

    feature(area-citas):Pagina Cita con sus funciones (#4)
    Se creo la Pagina para citas.
    Se agregaron las funciones para la pagina de citas atraves de JavaScript.

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\LoginController;
+use Controllers\CitaController;
 $router = new Router();
 
 //Iniciar Sesión
@@ -25,6 +26,9 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
 
+// Area Privada
+$router->get('/cita', [CitaController::class, 'index']);
+$router->post('/cita', [CitaController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
